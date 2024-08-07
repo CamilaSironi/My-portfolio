3421d79653157c3390016ad31a05ac574324c7c2
Camila Sironi <sironicamila98@gmail.com>
2024-08-06 22:15:14 -0300

    .

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,12 @@
-import foto1 from '../images/linkedin-logo.png'
-import foto2 from '../images/github-logo.png'
+import { GithubOutlined, LinkedinOutlined } from "@ant-design/icons"
+import { Button } from "antd"
 
 function Footer() {
     return(
         <div className="footer">
             <p>My networks:</p>
-            <div><a href="https://www.linkedin.com/in/camila-sironi-5b6298227/" target="_blank"><img className="footer-photo" src={foto1} alt="linkedin-logo"></img></a></div>
-            <div><a href="https://github.com/CamilaSironi" target="_blank"><img className="footer-photo" src={foto2} alt="github-logo"></img></a></div>
+            <Button type="link" href="https://www.linkedin.com/in/camila-sironi-5b6298227/" target="_blank" icon={<LinkedinOutlined />} size="large" />
+            <Button type="link" href="https://github.com/CamilaSironi" target="_blank" icon={<GithubOutlined/>} size="large" />
         </div>
     )
 }
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,25 +1,21 @@
-import logo1 from '../images/html-logo.png'
-import logo2 from '../images/css-logo.png'
-import logo3 from '../images/js-logo.png'
-import logo4 from '../images/java-logo.png'
-import logo5 from '../images/php-logo.png'
-import logo6 from '../images/sql-logo.png'
-import logo7 from '../images/git-logo.png'
-import logo8 from '../images/react-logo.png'
+import { Flex, Tag } from 'antd'
 
 function Skills() {
     return(
         <div id="skills">
             <h2>My main skills include...</h2>
             <div className='skill-container'>
-                <img src={logo1} alt="html-logo"></img>
-                <img src={logo2} alt="css-logo"></img>
-                <img src={logo3} alt="js-logo"></img>
-                <img src={logo4} alt="java-logo"></img>
-                <img src={logo5} alt="php-logo"></img>
-                <img src={logo6} alt="sql-logo"></img>
-                <img src={logo7} alt="git-logo"></img>
-                <img src={logo8} alt="react-logo"></img>
+                <Flex gap="20px 6px" wrap>
+                    <Tag color="geekblue">HTML</Tag>
+                    <Tag color="geekblue">CSS</Tag>
+                    <Tag color="geekblue">JAVASCRIPT</Tag>
+                    <Tag color="geekblue">JAVA</Tag>
+                    <Tag color="geekblue">PHP</Tag>
+                    <Tag color="geekblue">SQL</Tag>
+                    <Tag color="geekblue">REACT</Tag>
+                    <Tag color="geekblue">EXPRESS</Tag>
+                    <Tag color="geekblue">NODE</Tag>
+                </Flex>
             </div>
             <h2 className='bottom-text'>...and always willing to learn more!</h2>
         </div>
