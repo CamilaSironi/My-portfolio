333b6f360c777004cad64d750befbb84aab7cc2e
Camila Sironi <sironicamila98@gmail.com>
2024-08-07 18:18:26 -0300

    added a project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,10 +1,24 @@
+import { Card } from "antd"
+import Meta from "antd/es/card/Meta"
+import img from "../images/project1.png"
+import Link from "antd/es/typography/Link"
+
 function Projects() {
     return(
         <div id="projects">
             <h2>Discover my latest projects:</h2>
-            {/*<div><a href=""><img className="project-photo" src={} alt=""></img></a></div>
-            <div><a href=""><img className="project-photo" src={} alt=""></img></a></div>
-            <div><a href=""><img className="project-photo" src={} alt=""></img></a></div>*/}
+            <Link href={'https://movies-front-five.vercel.app'} target="_blank">
+                <Card
+                    hoverable
+                    bordered={true}
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src={img} />}
+                >
+                    <Meta title="Movies Page" description="movies-front-five.vercel.app" />
+                </Card>
+            </Link>
         </div>
     )
 }
