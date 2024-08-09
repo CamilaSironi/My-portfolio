b0e3532df6e74e43b398d0ba7fccff85f4d670a0
Camila Sironi <sironicamila98@gmail.com>
2024-08-08 21:56:02 -0300

    changes in skill list style

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,18 +7,20 @@ function Projects() {
     return(
         <div id="projects">
             <h2>Discover my latest projects:</h2>
-            <Link href={'https://movies-front-five.vercel.app'} target="_blank">
-                <Card
-                    hoverable
-                    bordered={true}
-                    style={{
-                        width: 240,
-                    }}
-                    cover={<img alt="example" src={img} />}
-                >
-                    <Meta title="Movies Page" description="movies-front-five.vercel.app" />
-                </Card>
-            </Link>
+            <div className="card">
+                <Link href={'https://movies-front-five.vercel.app'} target="_blank">
+                    <Card
+                        hoverable
+                        bordered={true}
+                        style={{
+                            width: 240,
+                        }}
+                        cover={<img alt="example" src={img} />}
+                    >
+                        <Meta title="Movies Page" description="movies-front-five.vercel.app" />
+                    </Card>
+                </Link>
+            </div>
         </div>
     )
 }
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,21 +1,50 @@
-import { Flex, Tag } from 'antd'
+import {List} from 'antd'
 
 function Skills() {
+
+    const data = [
+        {
+            title: 'HTML',
+        },
+        {
+            title: 'CSS',
+        },
+        {
+            title: 'Javascript',
+        },
+        {
+            title: 'JAVA',
+        },
+        {
+            title: 'PHP',
+        },
+        {
+            title: 'SQL',
+        },
+        {
+            title: 'React.js',
+        },
+        {
+            title: 'Express',
+        },
+        {
+            title: 'Node.js',
+        }
+      ];
+
     return(
         <div id="skills">
             <h2>My main skills include...</h2>
             <div className='skill-container'>
-                <Flex gap="20px 6px" wrap>
-                    <Tag color="geekblue">HTML</Tag>
-                    <Tag color="geekblue">CSS</Tag>
-                    <Tag color="geekblue">JAVASCRIPT</Tag>
-                    <Tag color="geekblue">JAVA</Tag>
-                    <Tag color="geekblue">PHP</Tag>
-                    <Tag color="geekblue">SQL</Tag>
-                    <Tag color="geekblue">REACT</Tag>
-                    <Tag color="geekblue">EXPRESS</Tag>
-                    <Tag color="geekblue">NODE</Tag>
-                </Flex>
+                <List
+                    grid={{gutter: 16}}
+                    dataSource={data}
+                    renderItem={(item) => (
+                    <List.Item>
+                        <div className='skill-item'>{item.title}</div>
+                    </List.Item>
+                    )}
+                />
             </div>
             <h2 className='bottom-text'>...and always willing to learn more!</h2>
         </div>
