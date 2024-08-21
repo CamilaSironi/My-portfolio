1f9a6062ecdeb5911933efac90a561671fdcf433
Camila Sironi <sironicamila98@gmail.com>
2024-08-20 22:01:18 -0300

    added project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,25 +1,44 @@
 import { Card } from "antd"
 import Meta from "antd/es/card/Meta"
-import img from "../images/project1.png"
+import img1 from "../images/project1.png"
+import img2 from "../images/project2.png"
 import Link from "antd/es/typography/Link"
 
 function Projects() {
     return(
         <div id="projects">
             <h2>Discover my latest projects:</h2>
-            <div className="card">
-                <Link href={'https://movies-front-five.vercel.app'} target="_blank">
-                    <Card
-                        hoverable
-                        bordered={true}
-                        style={{
-                            width: 240,
-                        }}
-                        cover={<img alt="example" src={img} />}
-                    >
-                        <Meta title="Movies Page" description="movies-front-five.vercel.app" />
-                    </Card>
-                </Link>
+            <div className="card-container">
+                <div className="card">
+                    <Link href={'https://movies-front-five.vercel.app'} target="_blank">
+                        <Card
+                            hoverable
+                            bordered={true}
+                            style={{
+                                width: 240,
+                            }}
+                            cover={<img alt="example" src={img1} />}
+                            type='flex'
+                        >
+                            <Meta title="Movies Page" description="movies-front-five.vercel.app" />
+                        </Card>
+                    </Link>
+                </div>
+                <div className="card">
+                    <Link href={'https://peluqueria-pink.vercel.app'} target="_blank">
+                        <Card
+                            hoverable
+                            bordered={true}
+                            style={{
+                                width: 240,
+                            }}
+                            cover={<img alt="example" src={img2} />}
+                            type='flex'
+                        >
+                            <Meta title="Peluqueria" description="https://peluqueria-pink.vercel.app" />
+                        </Card>
+                    </Link>
+                </div>
             </div>
         </div>
     )
