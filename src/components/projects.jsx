856507291001f4e3bc2ef9e75a1523425eb1d32e
Camila Sironi <sironicamila98@gmail.com>
2024-09-03 16:39:38 -0300

    add new project, fix mobile responsiveness

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,6 +2,7 @@ import { Button, Card } from "antd"
 import Meta from "antd/es/card/Meta"
 import img1 from "../images/project1.png"
 import img2 from "../images/project2.png"
+import img3 from "../images/project3.png"
 import Link from "antd/es/typography/Link"
 import { GithubOutlined, LinkOutlined} from "@ant-design/icons"
 
@@ -15,9 +16,6 @@ function Projects() {
                         <Card
                             hoverable
                             bordered={true}
-                            style={{
-                                width: 300
-                            }}
                             cover={<img alt="example" src={img1} />}
                             type='flex'
                         >
@@ -40,9 +38,6 @@ function Projects() {
                         <Card
                             hoverable
                             bordered={true}
-                            style={{
-                                width: 300
-                            }}
                             cover={<img alt="example" src={img2} />}
                             type='flex'
                         >
@@ -58,6 +53,29 @@ function Projects() {
                         <p>Site for a hairdresser&apos;s salon.<em>- Still in process</em> </p> 
                         <Button className="btn" type="link" href="https://github.com/CamilaSironi/peluqueria" target="_blank" icon={<GithubOutlined/>} size="large">Code</Button>
                         <Button className="btn" type="link" href="https://peluqueria-pink.vercel.app/" target="_blank" icon={<LinkOutlined />} size="large">Preview</Button>  
+                    </div>
+                </div>
+
+                <div className="card">
+                    <Link href={'https://memes-app.vercel.app/'} target="_blank">
+                        <Card
+                            hoverable
+                            bordered={true}
+                            cover={<img alt="example" src={img3} />}
+                            type='flex'
+                        >
+                            <Meta title="Meme generator" description="https://memes-app.vercel.app/" />
+                        </Card>
+                    </Link>
+                    <div className="description">
+                        <h2>Meme generator</h2>
+                        <Button className="project-skill" type="primary">React.js</Button>
+                        <Button className="project-skill" type="primary">Javascript</Button>
+                        <Button className="project-skill" type="primary">CSS</Button>
+                        <Button className="project-skill" type="primary">HTML</Button>
+                        <p>Page to create and download memes.</p> 
+                        <Button className="btn" type="link" href="https://github.com/CamilaSironi/MemesApp" target="_blank" icon={<GithubOutlined/>} size="large">Code</Button>
+                        <Button className="btn" type="link" href="https://memes-app.vercel.app/" target="_blank" icon={<LinkOutlined />} size="large">Preview</Button>  
                     </div>
                 </div>
             </div>
